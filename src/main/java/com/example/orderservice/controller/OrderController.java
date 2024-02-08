57d100f1642fc5a00a8c47eae0ff5f3fd1fcecab
Inseong Kim <isk1149@naver.com>
2024-02-08 20:52:07 +0900

    update

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
@@ -24,6 +27,8 @@ import java.util.List;
 public class OrderController {
     private final Environment env;
     private final OrderService orderService;
+//    private final KafkaProducer kafkaProducer;
+//    private final OrderProducer orderProducer;
 
     @GetMapping("/health_check")
     public String status() {
@@ -43,6 +48,17 @@ public class OrderController {
         /* jpa */
         OrderDto createdOrder = orderService.createOrder(orderDto);
         ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+
+        /* kafka */
+//        orderDto.setOrderId(UUID.randomUUID().toString());
+//        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+
+
+        /* send an order to the kafka */
+//        kafkaProducer.send("example-order-topic", orderDto);
+//        orderProducer.send("orders", orderDto);
+
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
 
         log.info("After added orders data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
